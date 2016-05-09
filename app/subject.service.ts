386fa5137a7207f524247da28669d2068368292f
patawat <patawat.wtk@gmail.com>
2016-05-09 22:35:33 +0700

    fix bug on subject service

--- app/subject.service.ts
+++ app/subject.service.ts
@@ -1,9 +1,8 @@
 import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http'
-
 import {SubjectList} from './subjectList';
-import {SubjectDetail} from './SubjectDetail';
+import {SubjectDetail} from './subjectDetail';
 import { Observable }     from 'rxjs/Observable';
 
 
